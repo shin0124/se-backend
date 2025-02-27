d023344a0201fed6636e6879acf297c0344518d7
tensept <tensept23@gmail.com>
2025-02-27 19:09:06 +0700

    edit some data

--- src/patient/patient.entity.ts
+++ src/patient/patient.entity.ts
@@ -14,9 +14,6 @@ export class Patient {
   @Column({ nullable: true })
   age: number;
 
-  @Column({ nullable: false })
-  HN: number;
-
   @OneToMany(() => Diary, (diary) => diary.patient)
   diaries: Diary[];
 
